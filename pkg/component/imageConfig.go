21f7bf97a5db9ffc607ea37c2822d4c1c7f4eb10
bob.huang <bob.huang@prophetstor.com>
2020-02-06 16:41:17 +0800

    Change default image for federatorai-agent-app.
    Signed-off-by: bob.huang <bob.huang@prophetstor.com>

--- pkg/component/imageConfig.go
+++ pkg/component/imageConfig.go
@@ -23,8 +23,7 @@ const (
 	defaultImageFedemeterInfluxdb         = "quay.io/prophetstor/fedemeter-influxdb:v0.3.39"
 	defaultImageFrontend                  = "quay.io/prophetstor/federatorai-dashboard-frontend:v4.2.702"
 	defaultImageBackend                   = "quay.io/prophetstor/federatorai-dashboard-backend:v4.2.702"
-	// TODO:
-	defaultImageFederatoraiAgentApp = "quay.io/prophetstor/federatorai-agent-app:v4.2.512"
+	defaultImageFederatoraiAgentApp       = "repo.prophetservice.com/federatorai-agent-app:v0.9.148"
 )
 
 type ImageConfig struct {
